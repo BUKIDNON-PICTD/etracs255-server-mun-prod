b504f14a8a3a8b8988ce7b12e3a30c069cea9b3d
RUFINO JOHN AGUILAR <aguilarufino@gmail.com>
2019-09-25 16:32:04 +0800

    fix form 60

--- workspace/services/apps/modules/rameses-gov-treasury-municipality-services.jar/sql/form60report.sql
+++ workspace/services/apps/modules/rameses-gov-treasury-municipality-services.jar/sql/form60report.sql
@@ -6,10 +6,10 @@ from form60setup fi
 where fi.type='item'
 order by h.code, fi.code 
 
-[getLiquidatedSummary]
+[getLiquidatedSummary1]
 select 
    fa.parentid, sum(cri.amount) as total 
-from liquidation l
+from collection_voucher l
   inner join liquidation_remittance lr on lr.liquidationid = l.objid 
   inner join remittance_cashreceipt rc on rc.remittanceid = lr.objid 
   inner join cashreceipt c on c.objid = rc.objid 
@@ -19,3 +19,17 @@ from liquidation l
 where  l.dtposted between $P{fromdate} and $P{todate}  
     and cv.objid is null 
 group by fa.parentid 
+
+[getLiquidatedSummary]
+SELECT 
+   fa.parentid, SUM(cri.amount) AS total 
+FROM collectionvoucher l
+  INNER JOIN remittance lr ON lr.collectionvoucherid = l.objid 
+  INNER JOIN cashreceipt rc ON rc.remittanceid = lr.objid 
+  INNER JOIN cashreceipt c ON c.objid = rc.objid 
+  INNER JOIN cashreceiptitem cri ON cri.receiptid = c.objid
+  INNER JOIN form60account fa ON fa.revenueitemid = cri.item_objid 
+  LEFT JOIN cashreceipt_void cv ON cv.receiptid = c.objid 
+WHERE  YEAR(l.dtposted)=2019
+    AND cv.objid IS NULL 
+GROUP BY fa.parentid 
